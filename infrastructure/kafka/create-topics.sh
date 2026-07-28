ba7701a9379ed72ffef910305d2923e0f9578317
Sergey Smolnikov <ssmol@equinor.com>
2026-07-28 13:05:55 +0200

    init

--- infrastructure/kafka/create-topics.sh
+++ infrastructure/kafka/create-topics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-telemetry --partitions 1 --replication-factor 1
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-state-events --partitions 1 --replication-factor 1
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-command-events --partitions 1 --replication-factor 1
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-emergency-events --partitions 1 --replication-factor 1
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-telemetry --partitions 1 --replication-factor 1
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-state-events --partitions 1 --replication-factor 1
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-command-events --partitions 1 --replication-factor 1
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists --topic robot-emergency-events --partitions 1 --replication-factor 1
